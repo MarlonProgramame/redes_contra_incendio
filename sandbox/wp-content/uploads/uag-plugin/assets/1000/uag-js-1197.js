0abb12e494b90eef32b5213ce64cea3131d8bfec
Marlon Arshumi <marlonstevenaristizabal@gmail.com>
2025-07-25 13:02:27 -0500

    Ajustes en el footer

--- sandbox/wp-content/uploads/uag-plugin/assets/1000/uag-js-1197.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	UAGBForms.init( {"block_id":"9eefc7a2","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":false,"afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Env\u00edo del formulario","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Por favor, completa el captcha de arriba.","confirmationUrl":""}, '.uagb-block-9eefc7a2', 1197 );
-});
- });
